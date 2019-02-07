c503f981409015ecb38cb91898605769e45ba26b
Malcolm Durling <malcolm@durling.co.nz>
2019-02-07 14:59:03 +1300

    Add init method for Landmarks SDk

--- ios/RNLandmarks.m
+++ ios/RNLandmarks.m
@@ -1,13 +1,26 @@
 
 #import "RNLandmarks.h"
 
+@import LandmarksIDiOS;
+
+
 @implementation RNLandmarks
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(initialize:(NSString *)appId appSecret:(NSString *)appSecret apiKey:(NSString *)apiKey)
+{
+  LandmarksIDManagerDelegate* landmarksManager = [LandmarksIDManagerDelegate initialize: appId appSecret: appSecret apiKey: apiKey];
+  
+  [landmarksManager setup];
+  
+  [LandmarksIDLocationManager getInstance].locationDelegate = landmarksManager;
+  [LandmarksIDLocationManager getInstance].sessionDelegate  = landmarksManager;
+}
+
 
 @end
   
