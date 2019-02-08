e8c9ad9a08ab4a3fd7d41583397792cb9c9ea4f2
Malcolm Durling <malcolm@durling.co.nz>
2019-02-08 15:23:39 +1300

    Add additional methods

--- ios/RNLandmarks.m
+++ ios/RNLandmarks.m
@@ -1,8 +1,17 @@
+//
+//  CashRewards
+//
+//  Created by Malcolm Durling on 08/02/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
 
-#import "RNLandmarks.h"
+#import <Foundation/Foundation.h>
+
 #import <React/RCTConvert.h>
+#import "RNLandmarks.h"
 
 @import LandmarksIDiOS;
+
 
 @implementation RNLandmarks
 
@@ -10,15 +19,15 @@ RCT_EXPORT_MODULE();
 
 - (dispatch_queue_t)methodQueue
 {
-    return dispatch_get_main_queue();
+  return dispatch_get_main_queue();
 }
 
 RCT_EXPORT_METHOD(initialize: (NSDictionary *)config)
 {
-  NSString* appId = [RCTConvert NSString:config[@"appId"]]; 
-  NSString* appSecret = [RCTConvert NSString:config[@"appSecret"]]; 
-  NSString* apiKey = [RCTConvert NSString:config[@"apiKey"]]; 
-
+  NSString* appId = [RCTConvert NSString:config[@"appId"]];
+  NSString* appSecret = [RCTConvert NSString:config[@"appSecret"]];
+  NSString* apiKey = [RCTConvert NSString:config[@"apiKey"]];
+  
   LandmarksIDManagerDelegate* landmarksManager = [LandmarksIDManagerDelegate initialize: appId appSecret: appSecret apiKey: apiKey];
   
   [landmarksManager setup];
@@ -27,6 +36,21 @@ RCT_EXPORT_METHOD(initialize: (NSDictionary *)config)
   [LandmarksIDLocationManager getInstance].sessionDelegate  = landmarksManager;
 }
 
+RCT_EXPORT_METHOD(startTracking)
+{
+  [[LandmarksIDManagerDelegate sharedManager] startTracking];
+}
+
+RCT_EXPORT_METHOD(stopTracking)
+{
+  [[LandmarksIDManagerDelegate sharedManager] stopTracking];
+}
+
+RCT_EXPORT_METHOD(requestLocationPermissions)
+{
+  [[LandmarksIDManagerDelegate sharedManager] requestLocationPermissions];
+}
+
 
 @end
-  
+
